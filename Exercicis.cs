ba58978550f058a7138278bea11370ebf626b4db
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2024-11-19 13:00:01 +0100

    Exercici 13 Finished Closes #7

---
+++ Exercicis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MyLibraryAleMar;
+namespace ExercicisTema2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const string InsertaNum = "Inserta un nombre enter de minuts";
+            const string TeuNum = "Els teus minuts a segons son: {0}s";
+            const string Error = "El valor ha de ser numeric";
+            int NumUser;
+            Console.WriteLine(InsertaNum);
+            try
+            {
+                NumUser = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(TeuNum, Utils.MinutesToSeconds(NumUser));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(Error);
+            }
+        }
+    }
+}
